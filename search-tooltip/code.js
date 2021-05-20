c37c5c9b9335e4c4edde0d30cc5211267ababf6d
pranav1601 <pranav.agarw@gmail.com>
2021-05-20 07:49:19 +0530

    added code for title(tooltip) which displays the top 25

--- search-tooltip/code.js
+++ search-tooltip/code.js
@@ -4,6 +4,30 @@ const url = 'https://www.airbus.com/';
 rp(url)
   .then(function(html){
     //success!
+    t=" title="
+    titlearray=[]
+    for (let i = 0; i < html.length-7; i++) {
+        if(html.slice(i,i+7)===t){
+            var j=i
+            var count=0
+            var init=-1
+            var final=-1
+            while(j<html.length && count<2){
+                if(html.charAt(j)==="\""){
+                    count=count+1
+                    if(count===1){
+                        init=j
+                    }
+                    else{
+                        final=j
+                    }
+                }
+                j=j+1
+            }
+            titlearray.push(html.slice(init+1,final))
+        }
+      }
+    // console.log(titlearray)
     someText = html.replace(/(\r\n|\n|\r)/gm, "");
     // final=someText.replace(/\s/g,'')
     // finalfinal=final.replace( /(<([^>]+)>)/ig, '')
@@ -16,9 +40,15 @@ rp(url)
     let finalres = res.filter(function (temp) {
         return reg_name_lastname.test(temp) && temp.length>3 && !temp.includes('\n') && !temp.includes('\r')})
     let uniqueArray = [...new Set(finalres)];
-    console.log(uniqueArray)
+
+    var finalArray=titlearray.concat(uniqueArray)
+    if(finalArray.length<25){
+        finalArray=finalArray.slice(0,finalArray.length)
+    }else{
+        finalArray=finalArray.slice(0,25)
+    }
+    console.log(finalArray)
     
   })
   .catch(function(err){
-    //handle error
   });
